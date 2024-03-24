7d39c7f74c798469603896f9c25605d8029c7e4b
Petros Devrikis <100411628+Petrosdevri@users.noreply.github.com>
2024-03-24 16:14:45 +0000

    Added Romfea scraper

---
+++ news/gr/romfea.js
@@ -0,0 +1,25 @@
+const puppeteer = require('puppeteer');
+
+const url = 'https://www.romfea.gr/';
+
+async function extract() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const news = await page.evaluate(() => {
+        let newsArr = [];
+        let titles = Array.from(document.querySelectorAll('div a'));
+        
+        for (const title of titles) {
+            newsArr.push({ Title: title.textContent.trim(), Link: title.href });
+        }
+
+        return newsArr;
+    });
+
+    await browser.close();
+    console.log(news);
+}
+
+extract();
